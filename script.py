66791bda6779c5af8c1693df80c9dece7f37bf31
grobocop12 <chasnik97876@gmail.com>
2019-04-10 10:57:01 +0200

    frame counter

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import cv2
 import numpy as np
 import sys
 import os.path
+import pickle
 
 def print_help():
     print(f'Usage:')
@@ -11,20 +13,28 @@ def main_loop(file_name):
 
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     capture = cv2.VideoCapture(file_name)
-
+    length = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(length)
+    i = 1
     while(capture.isOpened()):
         ret, frame = capture.read()
-
-        if(frame is not None):
-            faces = face_cascade.detectMultiScale(frame,scaleFactor= 1.2, minNeighbors=3)#,minSize= (20,20))
-            #print('Detected faces:', len(faces))
+        print(f'{i}/{length}',end='\r')
+        i+=1
+        if ret:
+            faces = face_cascade.detectMultiScale(frame,scaleFactor= 1.4, minNeighbors=3)#,minSize= (20,20))
+            
             for (x,y,w,h) in faces:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            
+        if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
             break
     capture.release()
     cv2.destroyAllWindows()
+    print()
+
+def save_faces(faces):
+    print()
 
 def main():
     if len(sys.argv)<2:
