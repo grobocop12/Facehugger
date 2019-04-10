36f9d14a9ac13adce7485e241fad7f5e7c260d8d
grobocop12 <chasnik97876@gmail.com>
2019-04-10 13:39:21 +0200

    serialization script

--- Extractor.py
+++ Extractor.py
@@ -6,43 +6,41 @@ import os.path
 import pickle
 
 def print_help():
+    print('Face Extractor')
     print(f'Usage:')
-    print(f'{sys.argv[0]} <video file name>')
+    print(f'Extractor.py <video file name> <output file name>')
 
-def main_loop(file_name):
+def main_loop(src_file, output):
 
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-    capture = cv2.VideoCapture(file_name)
+    capture = cv2.VideoCapture(src_file)
     length = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(length)
-    i = 1
+    file = open(output,'ab')
+    i = 0
     while(capture.isOpened()):
         ret, frame = capture.read()
         print(f'{i}/{length}',end='\r')
         i+=1
         if ret:
-            faces = face_cascade.detectMultiScale(frame,scaleFactor= 1.4, minNeighbors=3)#,minSize= (20,20))
-            
+            faces = face_cascade.detectMultiScale(frame,scaleFactor= 1.4, minNeighbors=3)
             for (x,y,w,h) in faces:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.imshow('frame',frame)
-            
-        if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
+                save_face(frame[y:(y+h),x:(x+w)],file)
+        else:
             break
     capture.release()
-    cv2.destroyAllWindows()
+    file.close()
     print()
 
-def save_faces(faces):
-    print()
+def save_face(face, file):
+    pickle.dump(face,file)
 
 def main():
-    if len(sys.argv)<2:
+    if len(sys.argv)<3:
         print_help()
         sys.exit()
 
     if(os.path.isfile(sys.argv[1])):
-        main_loop(sys.argv[1])
+        main_loop(sys.argv[1],sys.argv[2])
     else:
         print('No such file!')
         print_help()
