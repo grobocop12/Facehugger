b1e92da94a68510fdfbe7df89209a64459aa6016
grobocop <chasnik97876@gmail.com>
2019-04-28 16:21:50 +0200

    gui source file is now set in command line

--- gui/gui-test.py
+++ gui/gui-test.py
@@ -1,6 +1,8 @@
 import kivy
 kivy.require('1.9.1')
 
+import sys
+import os
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
@@ -13,9 +15,15 @@ class VideoPlayerApp(App):
     def build(self):
 
         self.player = VideoPlayer()
-
+        self.player.source = sys.argv[1]
         return (self.player)
 
 
 if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print('You have to pass the path to source file')
+        sys.exit()
+    if not os.path.isfile(sys.argv[1]):
+        print('Error: file not found')
+        sys.exit()
     VideoPlayerApp().run()
