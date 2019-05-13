31ef0ef8b34249437aa7ea67181112bf2e05ae61
sivietz <kasiarzyna.siwiec@gmail.com>
2019-05-13 02:11:44 +0200

    file explorer
    1. choose file
    2. press that big-ass play button

--- gui/gui-test.py
+++ gui/gui-test.py
@@ -1,4 +1,5 @@
 import kivy
+import easygui
 kivy.require('1.9.1')
 
 import sys
@@ -6,17 +7,58 @@ import os
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 from kivy.uix.videoplayer import VideoPlayer
+from kivy.properties import StringProperty
 
+
+class Grid(GridLayout):
+    def __init__(self, **kwargs):
+        super(Grid, self).__init__(**kwargs)
+
+
+        self.inside_grid = GridLayout()
+        self.inside_grid.rows = 2
+        #self.inside_grid.cols = 3
+
+        self.rows = 3
+        self.player = VideoPlayer()
+        self.add_widget(self.player)
+
+        self.open_explorer = Button(text="Select file", font_size=40)
+        self.open_explorer.bind(on_press=self.openexp)
+        self.inside_grid.add_widget(self.open_explorer)
+
+        self.play_video = Button(text="Play", font_size=40)
+        self.play_video.bind(on_press=self.playvid)
+        self.inside_grid.add_widget(self.play_video)
+
+
+        self.add_widget(self.inside_grid)
+
+    def openexp(self, instance):
+        self.file = easygui.fileopenbox()
+        print(self.file)
+
+    def playvid(self, instance):
+        self.player.source = str(self.file)
+        if self.player.source.lower().endswith(('.mp4', '.avi', '.mkv', '.flv', '.wmv')):
+            print(self.player.source)
+            self.player.state = 'play'
+        else:
+            print('this should be an error i guess')
 
 class VideoPlayerApp(App):
-
     def build(self):
-
-        self.player = VideoPlayer()
-        self.player.source = sys.argv[1]
-        return (self.player)
+        return Grid()
 
 
 if __name__ == '__main__':
@@ -27,3 +69,5 @@ if __name__ == '__main__':
         print('Error: file not found')
         sys.exit()
     VideoPlayerApp().run()
+
+
