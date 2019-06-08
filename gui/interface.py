ec46dd631d6c5ae0d07092ce26b222aedff3d2a0
sivietz <kasiarzyna.siwiec@gmail.com>
2019-06-08 21:27:57 +0200

    automatic list selection and submitting with enter

--- gui/interface.py
+++ gui/interface.py
@@ -8,6 +8,8 @@ from kivy.properties import StringProperty
 import kivy
 import easygui
 import copy
+import Person
+import RecognizerGUi
 kivy.require("1.9.0")
 
 
@@ -30,10 +32,13 @@ class Faces(BoxLayout):
     picture_list = []
     face_list = ObjectProperty()
     info = StringProperty()
+    persons = []
 
     # method called by "Train" button
     def train(self):
         print("training!")
+        RecognizerGUi.Start(self.persons)
+
 
     # adding names to list
     def submit_face(self):
@@ -48,6 +53,10 @@ class Faces(BoxLayout):
             if not check_name:
                 self.face_list.adapter.data.extend([face_name])
                 self.face_list._trigger_reset_populate()
+                index = len(self.face_list.adapter.data) - 1
+                if not self.face_list.adapter.get_view(index).is_selected:
+                    self.face_list.adapter.get_view(index).trigger_action(duration=0)
+
         else:
             self.info = "name cannot be empty"
 
@@ -60,7 +69,6 @@ class Faces(BoxLayout):
             if faces:
                 faces.pop(Faces.get_face_index(selection))
             self.face_list._trigger_reset_populate()
-            #self.face_list.adapter.selection = None
         else:
             self.info = "select item to delete"
 
@@ -74,26 +82,22 @@ class Faces(BoxLayout):
             # check file extensions
             if self.picture_list:
                 self.picture_list = [x for x in self.picture_list if x.endswith('jpg') or x.endswith('png')]
-                # create new objects
-                if self.picture_list:
-                    for i in range(len(faces)):
-                        if faces[i].name == selection:
-                            faces.pop(Faces.get_face_index(selection))
-                            self.info = "item has been overwritten"
-
-                    selected_face = Face(selection, self.picture_list)
-                    faces.append(copy.copy(selected_face))
-                else:
-                    self.info = "no useful files found"
             else:
                 self.info = "no pictures have been selected"
         else:
             self.info = "select item to upload pictures"
-            try:
-                selected_face = Face(selection, self.picture_list)
-                faces.append(copy.copy(selected_face))
-            except UnboundLocalError:
-                pass
+
+        # create new objects
+        if self.picture_list:
+            for i in range(len(faces)):
+                if faces[i].name == selection:
+                    faces.pop(Faces.get_face_index(selection))
+                    self.info = "item has been overwritten"
+
+            selected_face = Face(selection, self.picture_list)
+            faces.append(copy.copy(selected_face))
+        else:
+            self.info = "no useful files found"
 
         print("list: ")
         for i in range(len(faces)):
@@ -101,11 +105,23 @@ class Faces(BoxLayout):
             for j in range(len(faces[i].pictures)):
                 print(faces[i].pictures[j])
 
+        z = 0
+        self.persons =[]
+        for i in faces:
+            z = z + 1
+            photo = i.pictures
+            name = i.name
+            osoba = Person.Person(photo, name, z)
+            self.persons.append(osoba)
+
+
     def get_face_index(name):
         for i, f in enumerate(faces):
             if f.name == name:
                 return i
         return -1
+
+
 
 class InterfaceApp(App):
     def build(self):
